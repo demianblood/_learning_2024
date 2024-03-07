44ec2633e80a54d9bd2ab8d115691df4ecad1e26
demianblood <jokervanya2@gmail.com>
2024-03-07 17:07:01 +0200

    Init Commit

---
+++ JavaScriptModule/Task/1/Task1.js
@@ -0,0 +1,50 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+// const hello = "hello";
+// const owu = "owu";
+// const com = "com";
+// const ua = "ua";
+// const one = 1;
+// const ten = 10;
+// const minusNineNinetyNine = -999;
+// const OneTwoThree = 123;
+// const Pi =3.14;
+// const TwoSeven = 2.7;
+// const Sixten = 16;
+// const T = true;
+// const F = false;
+// console.log(hello);
+// console.log(owu);
+// console.log(com);
+// console.log(ua);
+// console.log(one);
+// console.log(ten);
+// console.log(minusNineNinetyNine);
+// console.log(OneTwoThree);
+// console.log(Pi);
+// console.log(TwoSeven);
+// console.log(Sixten);
+// console.log(T);
+// console.log(F);
+
+// let firstName = "Rusakovych";
+// let middleName = "Ivan";
+// let lastName = "Anatoliyovych";
+// let person = firstName + " " + middleName + " " + lastName;
+
+// let a = 100;
+    // let b = '100';
+    // let c = true;
+    // console.log(typeof a);
+    // console.log(typeof b);
+    // console.log(typeof c);
+
+// let PIB = "my name is " + prompt("write your name") + ", my surname is " + prompt("write your surname")+ ", my fathername is " + prompt("write your fathername") + ".";
+//
+// console.log(PIB);
